7c01fe6bbed1801034c907f982fe970639283adf
Rebeca Monis <>
2025-02-02 18:43:22 +0000

    docker

--- src/components/ChatbotComponent.js
+++ src/components/ChatbotComponent.js
@@ -3,8 +3,12 @@ import MessageParser from './ChatbotDeps/MessageParser.js';
 import ActionProvider from './ChatbotDeps/ActionProvider.js';
 import './ChatbotComponent.css'
 import Chatbot from 'react-chatbot-kit'
+import { useEffect } from 'react';
 
 const ChatbotComponent = () => {
+  useEffect(() => {
+    localStorage.clear()
+  }, [])
   return (
     <Chatbot
       config={config}
